13cd401e74a9f023cd66945e18ea3a7e339cd848
Ellen Mcmanis <emcmanis@reed.edu>
2012-05-06 14:51:09 -0700

    Added audioChunk.write(); set up getAudio.

--- audioChunk.java
+++ audioChunk.java
@@ -4,6 +4,13 @@ public class audioChunk{
     
     audioChunk(){
         format = new AudioFormat(44000,8,1,true,true);
-        data = new byte[32];
+        data = new byte[2048];
     }
 
+    int write(byte[] inputs; int inputs_offset; int outputs_offset; int len) {
+	int i;
+        for(i = 0; i <= len || i <= data.Length(); i++) {
+            data[i + outputs_offset] = inputs[i + inputs_offset];
+        }
+	return(i);
+    }
--- getAudio.java
+++ getAudio.java
@@ -1,42 +1,60 @@
+//obtains audio data from the computer's microphone, stores it in an audioChunk for future processing
+
 public class getAudio {
 
     TargetDataLine line;
-    AudioFormat format;
+    audioChunk output;
     
-    void setup(){
-        format = new AudioFormat(44000,8,1,true,true);
-        DataLine.Info info = new DataLine.Info(TargetDataLine.class, format); // format is an AudioFormat object
+    int setup(){
+        output = new audioChunk();
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class, output.format); // format is an AudioFormat object
         if (!AudioSystem.isLineSupported(Port.info.MICROPHONE)) {
             System.out.print("Something is wrong with your microphone.");
-            return;
+            return(-1);
         }
         // Obtain and open the line.
         try {
             line = (Port) AudioSystem.getLine(Port.Info.MICROPHONE);
-            line.open(format);
+            line.open(output.format);
         } catch (LineUnavailableException ex) {
             System.out.print("line is unavailable!");
-            return;
+            return(-1);
         }
+    return(0);
     }
 
-    void read() {
+    void process() {
         // Assume that the TargetDataLine, line, has already
         // been obtained and opened.
-        ByteArrayOutputStream out  = new ByteArrayOutputStream();
         int numBytesRead = 0;
+        int numBytesWrote = 0;
         byte[] data = new byte[line.getBufferSize() / 5];
         //byte[] audio = new byte[2048]; //for testing purposes -- recording and playing back some samples.
-    
+
         // Begin audio capture.
         line.start();
-    
         // Here, stopped is a global boolean set by another thread.
-        while (numBytesRead < 64) {
+        while (!stopped) {
+            //this loop starts with an input/output offset, writes as many bytes as possible, then loops. Once the audioChunk is full, it ffts it.
+
+            int output_offset = numBytesWrote;
+            int input_offset = 0;
             // Read the next chunk of data from the TargetDataLine.
-            numBytesRead = numBytesRead + line.read(data, 0, data.length);
-            // Save this chunk of data.
-            out.write(data, 0, numBytesRead);
+            numBytesRead = line.read(data, 0, data.length);
+            numBytesWrote = output.write(data, input_offset, output_offset, numBytesRead);
+            input_offset = input_offset + numBytesWrote;
+            
+            //handle the case where the audioChunk is full after the start of the loop
+            if(output_offset + numBytesWrote = output.data.Length()) {
+                //fft here
+            }
+            
+            //loop through data read (if necessary) and fft
+            while(input_offset != numBytesRead) {
+                numBytesWrote = output.write(data, input_offset, 0, numBytesRead); 
+                input_offset = input_offset + numBytesWrote;
+                //fft code goes here 
+            }
         }  
         return;
     }
