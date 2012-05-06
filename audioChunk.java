fe8eb1285600c1af789b3a2fa23d2d54112ca28f
Ellen Mcmanis <emcmanis@reed.edu>
2012-05-06 16:23:21 -0700

    changed array size for better resolution

--- audioChunk.java
+++ audioChunk.java
@@ -4,12 +4,12 @@ public class audioChunk{
     
     audioChunk(){
         format = new AudioFormat(44000,8,1,true,true);
-        data = new byte[2048];
+        data = new byte[4096];
     }
 
     int write(byte[] inputs; int inputs_offset; int outputs_offset; int len) {
 	int i;
-        for(i = 0; i <= len || i <= data.Length(); i++) {
+        for(i = 0; i + inputs_offset <= len || i + outputs_offset <= data.Length(); i++) {
             data[i + outputs_offset] = inputs[i + inputs_offset];
         }
 	return(i);
