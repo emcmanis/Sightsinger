bb798bc84e6fe0befc7d32c2290e830650ff2fb6
Ellen Mcmanis <emcmanis@reed.edu>
2012-05-30 12:47:32 -0700

    Continuing work on these files.

--- FFT.java
+++ FFT.java
@@ -1,44 +1,37 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+
 public class FFT {
 
     DoubleFFT_1D fft;
-    AudioChunk head;
+    LinkedBlockingQueue<AudioChunk> queue;
+    DataFrame frame;
 
-    public FFT() {
+    public FFT(DataFrame myframe) {
+        frame = myframe;
         fft = new DoubleFFT_1D(4096);
+        queue = new LinkedBlockingQueue<AudioChunk>();
     }
 
-    //linked list for a queue
 
-    public synchronized void enqueue(AudioChunk data) {
-        if(head == null) { //either this is the head
-            head = data;
-        }
-        else {  //or sticks on somewhere at the end
-            AudioChunk tail = head;
-            while(tail.next != null) {
-                tail = tail.next;
-            }
-            tail.next = data;
-
-        }
-        return;
-    }
-
-    private synchronized AudioChunk dequeue() {
-        AudioChunk res = head;
-        head = res.next;
-        return(res);
+    public void enqueue(AudioChunk data) {
+        queue.add(data);
     }
 
     //transforms data!
     public double[] transform(AudioChunk input) {
         double[] data = new double[4096];
-        //casting bytes to doubles. I think this is wrong.
-        for(int i = 0, i < 4096, i++) {
+        for(int i = 0; i < 4096; i++) {
             data[i] = (double)input[i];
         }
-
         fft.realForward(data);
+
+        //this loop performs the complex conjugation in order to get a power spectrum.
+        //we discard the data point at the nyquist frequency as its imaginary part is not available.
+        
+        for(int i = 1; i == data.length/2; i++) {
+            data[i] = (data[2*i] + data[2*i+1])*(data[2*i] - data[2*i+1]);
+        }
         return(data);
     } 
 
@@ -46,13 +39,17 @@ public class FFT {
 
         //again stopped is a global bool
 
-        while(!stopped) {
-            if(head != null) {
-                double[] data;
-                data = transform(dequeue());
-                // hand off to graphics thread
+        while(!frame.isStopped()) {
+            AudioChunk input;
+            try {
+                input = queue.take();
             }
+            catch(InterruptedException e) {
+                //HANDLE IT
+            }
+            double[] data;
+            data = transform(input);
+                // hand off to graphics thread
         }
     }
-
 }
